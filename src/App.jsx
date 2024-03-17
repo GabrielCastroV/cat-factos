6e895795aa0b4e304e9ff6c56eda89230d7446e9
Gabriel <gabrielenriquecas@gmail.com>
2024-03-17 11:22:37 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);          
-
       try {
         const res = await fetch(CAT_ENDPOINT_RANDOM_FACT);
         const data = await res.json();
